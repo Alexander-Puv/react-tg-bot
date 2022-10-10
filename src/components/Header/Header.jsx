ecb2eade3d6171888436ff8a281ddcca35f76f44
Alexander-Puv <sansano4sana@gmail.com>
2022-10-10 15:40:05 +0300

    initial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useTelegram } from '../../hooks/useTelegram';
 import { Button } from '../Button/Button';
 import cl from './Header.module.css';
 
 export const Header = () => {
     const {user, onClose} = useTelegram();
+    const navigate = useNavigate();
     
     return (
         <div className={cl.header}>
+            {window.location.pathname !== '/' ?
+                <Button onClick={() => navigate('/')}>Back</Button>
+            :
+                <></>
+            }
             <Button onClick={onClose}>Close</Button>
-            <span className={cl.username}>
-                {user?.first_name}
-            </span>
         </div>
     )
 }
